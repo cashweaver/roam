f83d41567a69d9c238314a988512960801575a33
Cash Weaver <cashbweaver@gmail.com>
2021-12-02 14:30:21 -0800

    Three nodes

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((org-hugo-base-dir . "~/proj/cashweaver.com")
+         (org-hugo-section . "posts")))
+ (org-mode . ((eval . (org-hugo-auto-export-mode)))))
